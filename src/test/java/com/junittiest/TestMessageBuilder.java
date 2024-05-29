04b292fd445e29fde817b37b83a6e1a7578e3601
NitishHS <hsnitish1@gmail.com>
2024-05-29 15:29:29 +0530

    Update TestMessageBuilder.java

--- src/test/java/com/junittiest/TestMessageBuilder.java
+++ src/test/java/com/junittiest/TestMessageBuilder.java
@@ -8,13 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMessageBuilder {
 
-    @Test
-    public void testNameMkyong() {
-
-        MessageBuilder obj = new MessageBuilder();
-        assertEquals("Hello mkyong", null);
-
-    }
+    
 
     @Test
     public void testNameEmpty() {
